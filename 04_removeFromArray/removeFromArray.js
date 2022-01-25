2f529be0e590791e9fc14b9bba57a43e5b1fc326
keshav278 <keshusuper@gmail.com>
2022-01-25 19:28:35 +0530

    solution added

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,5 +1,15 @@
-const removeFromArray = function() {
+const removeFromArray = function(array,...toDelete) {
+   for(let element of toDelete){
+       let index=array.indexOf(element);
+       if(index===-1)
+        continue;
+       else
+        {
+            array.splice(index,1);
+        }
 
+   }
+   return array;
 };
 
 // Do not edit below this line
--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -1,9 +1,19 @@
-const ftoc = function() {
-
+const ftoc = function(temp) {
+   let convertedTemp;
+   convertedTemp=5*(temp-32)/9;
+   if(5*(temp-32)%9===0)
+    return convertedTemp;
+   else
+    return Number(convertedTemp.toFixed(1)); 
 };
 
-const ctof = function() {
-
+const ctof = function(temp) {
+   let convertedTemp;
+   convertedTemp= temp*9/5+32;
+   if(temp*9%5===0)
+    return convertedTemp;
+   else
+     return Number(convertedTemp.toFixed(1));
 };
 
 // Do not edit below this line
