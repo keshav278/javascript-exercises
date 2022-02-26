c53e1778cdeb81b2be7dfcbe99a87e1bf8c460b4
keshav278 <keshusuper@gmail.com>
2022-02-26 18:08:45 +0530

    solutions added

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,6 +1,23 @@
-const palindromes = function () {
-
-};
+const palindromes = function (string) {
+        string=string.toLowerCase();
+        let seq=Array.from(string);
+        let s=[];
+        seq.forEach((char)=>{
+           if(char>='a'&&char<='z'){
+               s.push(char);
+           }
+        });
+        let palin=[];
+        for(char of s){
+            palin.push(char);
+        }
+        s.reverse();
+        for(let i=0;i<s.length;i++){
+            if(s[i]!=palin[i])
+             return false;
+        }
+        return true;
+    };        
 
 // Do not edit below this line
 module.exports = palindromes;
