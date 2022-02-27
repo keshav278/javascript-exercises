2e257c427654c3529d1429c3e4c4d77ba88a0434
keshav278 <keshusuper@gmail.com>
2022-02-27 22:07:42 +0530

    solutions added

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,4 +1,13 @@
-const fibonacci = function() {
+const fibonacci = function(num) {
+     let numm=Number(num);
+     if(numm===1)
+      return 1;
+     else if(numm===2)
+      return 1;
+     else if(numm<=0)
+      return "OOPS";  
+     else
+      return fibonacci(numm-1)+fibonacci(numm-2); 
 
 };
 
