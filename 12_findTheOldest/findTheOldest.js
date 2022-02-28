282814267799b7402a112ba68adf4f07b4f5633d
keshav278 <keshusuper@gmail.com>
2022-02-28 23:01:51 +0530

    solution added

--- 12_findTheOldest/findTheOldest.js
+++ 12_findTheOldest/findTheOldest.js
@@ -1,6 +1,17 @@
-const findTheOldest = function() {
-
-};
-
+const findTheOldest = function(list) {
+     list.forEach(function(obj){
+         if(!("yearOfDeath" in obj)){
+             obj.yearOfDeath= new Date().getFullYear();
+         }              
+     }
+     );
+     let maxAge = list.reduce((maxx,obj)=>Math.max(maxx,(obj.yearOfDeath-obj.yearOfBirth)),0);
+     let oldGuy = list.find(function(obj){
+         let age = obj.yearOfDeath-obj.yearOfBirth;
+         return age==maxAge;
+     });
+     return oldGuy;
+     
+    }
 // Do not edit below this line
 module.exports = findTheOldest;
